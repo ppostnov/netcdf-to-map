8fd09f40306ed5e4eab5dd7daaa11e70a91ef05d
nshevchu <71284477+nshevchu@users.noreply.github.com>
2020-09-27 17:35:06 +0300

    app update for new Mask clipper

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
 # from netcdf_mapper import downloader
 
+
+
+import logging
+
+logger = logging.getLogger('dev')
+logger.setLevel(logging.INFO)
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setLevel(logging.INFO)
+
+logger.addHandler(consoleHandler)
+
+formatter = logging.Formatter('%(asctime)s  %(name)s  %(levelname)s: %(message)s')
+consoleHandler.setFormatter(formatter)
+
+logger.info('информационное сообщение')
+
+from netcdf_mapper.maskclipper import MaskClipper
+
 # # the example how download the data for one year
 # d = downloader.FileRecipient()
 # d.download('2008', '2008-test')
@@ -9,8 +28,9 @@
 
 
 # Прописываем пути к файлам
-nc_path = "data\C3S-LC-L4-LCCS-Map-300m-P1Y-2018-v2.1.1.nc"
-shp_path = "data\\regions\szfo\Arkhangelskaya_oblast.shp"
+nc_path = r'..\storage\C3S-LC-L4-LCCS-Map-300m-P1Y-2018-v2.1.1.nc'
+shp_path = "data\\regions\szfo\Kaliningradskaya_oblast.shp"
+#shp_path = "data\\regions\szfo\Pskovskaya_oblast.shp"
 # Создаем обьект Клиппера и кидаем в него пути к файлам
 MC = MaskClipper(nc_path, shp_path)
 # Указываем какую переменную будем рисовать и для какого момента времени
@@ -20,10 +40,12 @@ MC.create_variable(variable='lccs_class', timevalue=0)
 # обрезаем датасет под интересующую область
 MC.subset()
 # обрезаем датасет под шейп
-#MC.clip()
-MC.every_clip()
+MC.clip()
+# считаем стату
+MC.statistic_calculation()
 # рисуем то, что получилось
-MC.draw_map()
+#MC.draw_map()
+MC.draw_beautiful_map()
 
 
 import netcdf_mapper
